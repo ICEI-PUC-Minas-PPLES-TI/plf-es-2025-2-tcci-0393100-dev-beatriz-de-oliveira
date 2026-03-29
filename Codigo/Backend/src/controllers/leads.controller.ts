bbd42a96c96b3a7337b22d5b80d6cef366f704bf
BeatrizOs1 <76188108+BeatrizOs1@users.noreply.github.com>
2026-03-29 19:34:38 -0300

    Delete Codigo/Backend/src/controllers directory

--- Codigo/Backend/src/controllers/leads.controller.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyReply, FastifyRequest } from "fastify";
-import { container } from "../modules/container.js";
-
-export async function listLeadsController(_request: FastifyRequest, reply: FastifyReply) {
-  const data = await container.leadsService.list();
-  return reply.send({ data });
-}
